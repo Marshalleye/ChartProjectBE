4b0db3c720dc87394fd4823678ded924b56eeac3
Nazar <Kapushchaknazar@gmail.com>
2022-07-08 21:49:19 -0500

    #3: Add corse and cookie-parser

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 import postRouter from "./postAPI/PostRouter.js";
 import fileUpload from "express-fileupload";
 import chartRouter from "./chartAPI/ChartRouter.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const DB_URL =
    "mongodb+srv://MarshallAdmin:ChartAdmin@cluster0.7jpgpld.mongodb.net/?retryWrites=true&w=majority";
 
@@ -15,16 +17,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static('static'));
+app.use(cors());
+app.use(cookieParser());
 app.use(fileUpload({}));
 app.use('/api', postRouter);
 app.use('/api', chartRouter);
-app.use(function (req, res, next) {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-   res.setHeader('Access-Control-Allow-Credentials', true);
-   next();
-});
 
 async function startApp() {
    try {
